24f51bac94147b62b5f5bb07bd850637a39dffa8
David Orson <onlyorson@outlook.com>
2020-08-04 13:11:21 +0100

    Reach Router + MVP Layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Router } from "@reach/router";
+
+import Home from "./Pages/Home";
+import Dashboard from "./Pages/Dashboard";
 
 const App = () => {
-  return <div>hi</div>;
+  return (
+    <Router>
+      <Home path="/" />
+      <Dashboard path="/dashboard" />
+    </Router>
+  );
 };
 
 export default App;
